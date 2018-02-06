6814ba6775075b0393b79652b2b007b915d392a5
lujianye <1260468819@qq.com>
2018-02-06 17:35:35 +0800

    创建V层与P层的基类

---
+++ app/src/main/java/com/lujianye/wanandroidstudy/mvp/base/BasePresenter.kt
@@ -0,0 +1,8 @@
+package com.lujianye.wanandroidstudy.mvp.base
+
+/**
+ * 基类---P层
+ * Created by cc_013 on 2018/2/6.
+ */
+class BasePresenter {
+}
---
+++ app/src/main/java/com/lujianye/wanandroidstudy/mvp/base/BaseView.kt
@@ -0,0 +1,7 @@
+package com.lujianye.wanandroidstudy.mvp.base
+
+/**
+ * 基类---V层
+ * Created by cc_013 on 2018/2/6.
+ */
+interface BaseView<T>
